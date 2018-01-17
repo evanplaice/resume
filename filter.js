961eddecba22907bb481cceddf7fdf5877662027
Evan Plaice <evanplaice@gmail.com>
2018-01-17 02:26:30 -0700

    Rewrote filter.js
    - gave existing properties more sensible names
    - reorganized sections[]
    - added employment filtering
    - added project filtering
    - rewrote the Object.filterByKey() helper

--- filter.js
+++ filter.js
@@ -1,19 +1,45 @@
-var add = [
+const sections = [
   'name',
+  'meta',
   'info',
   'contact',
+  // 'location',
   'employment',
   'projects',
   'skills',
-  'recognition'
+  // 'education',
+  // 'affiliation',
+  // 'service',
+  // 'disposition',
+  // 'writing',
+  // 'reading',
+  // 'speaking',
+  // 'governance',
+  'recognition',
+  // 'samples',
+  // 'social',
+  // 'references',
+  // 'testimonials',
+  // 'extracurricular',
+  // 'interests',
+  // 'languages';
 ];
 
-var exclude = [
-  'location',
-  'social',
-  'samples',
-  'reading',
-  'interests'
+const jobs = [
+  'WiseConnect Inc.',
+  //'24 Hour Alert Systems',
+  'Strike Group LLC.',
+  'Joel Plaice & Associates Inc.',
+  //'Spiral Aviation Training Company'
+];
+
+const projects = [
+  //'ng2-resume (OSS)',
+  'ng2-markdown (OSS)',
+  'node-ftpsync (OSS)',
+  'jquery-csv (OSS)',
+  'SharpPcap/Packet.Net (OSS)',
+  'Replacement Instructor Operating Station'
 ];
 
 // --------------------------
@@ -29,43 +55,83 @@ function main() {
 
   // read in the full resume
   var inputPath = path.join(__dirname, inputFile);
-  var input = JSON.parse(fs.readFileSync(inputPath, 'utf8'));
+  var resume = JSON.parse(fs.readFileSync(inputPath, 'utf8'));
 
-  // filter out unwanted fields
-  var output = trimResume(input);
+  // filter content
+  var resume = trimResume(resume, sections);
+  var resume = trimEmployment(resume, jobs);
+  var resume = trimProjects(resume, projects);
 
   // write the results to file
-  fs.writeFile(outputFile, JSON.stringify(output, null, 2), (err) => {
+  fs.writeFile(outputFile, JSON.stringify(resume, null, 2), (err) => {
     if (err) {
       return console.log(err);
     }
   });
 }
 
+// content filters
 // --------------------------
 
-// filter objects by their keys
-Object.filterByKey = function (obj, predicate) {
-  var result = {};
-  var key;
+Object.prototype.filterByKey = function (obj, predicate) {
+  return Object.keys(obj)
+    .filter(key => predicate(key))
+    .reduce((out, key) => {
+      out[key] = obj[key];
+      return out;
+    }, {});
+}
 
-  for (key in obj) {
-    if (obj.hasOwnProperty(key) && predicate(key)) {
-      result[key] = obj[key];
+
+// filter out unwanted sections
+function trimResume(resume, sections) {
+  return Object.filterByKey(resume, key => {
+    return sections.contains(key);
+  });
+  console.log(resume);
+}
+
+// list [num] number of employment entries
+function trimEmployment(resume, jobs) {
+  for (key in resume) {
+    if (key === "employment") {
+      resume[key].history = resume[key].history.filter((job) => {
+        return jobs.contains(job.employer);
+      });
     }
   }
+  return resume;
+}
 
-  return result;
-};
+function trimProjects(resume, projects) {
+  for (key in resume) {
+    if (key === "projects") {
+      resume[key] = resume[key].filter((project) => {
+        return projects.contains(project.title);
+      });
+    }
+  }
+  return resume;
+}
+
+// helpers
+// --------------------------
+
+Object.prototype.filterByKey = function (obj, predicate) {
+  return Object.keys(obj)
+    .filter(key => predicate(key))
+    .reduce((out, key) => {
+      out[key] = obj[key];
+      return out;
+    }, {});
+}
 
 // comparator to see if array contains an entry
 Array.prototype.contains = function(obj) {
   return this.indexOf(obj) > -1;
 };
 
-// filter out unwanted sections
-function trimResume(resume) {
-  return Object.filterByKey(resume, (key) => { return add.contains(key); });
-}
+// entry
+// --------------------------
 
 main();
