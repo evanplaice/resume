4efe676cf7f2114889ced31ecf68673e43b4dbae
Evan Plaice <evanplaice@gmail.com>
2018-01-17 13:47:24 -0700

    Renamed build:trim to build:filter to better reflect usage

--- src/setup.js
+++ src/setup.js
@@ -17,7 +17,7 @@ var package = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
 
 // build the scripts
 package.scripts['validate'] = `hackmyresume VALIDATE ${source}.json`;
-package.scripts['build:trim'] = `node ./src/filter.js ../${source} ./dist/tmp.json`;
+package.scripts['build:filter'] = `node ./src/filter.js ../${source} ./dist/tmp.json`;
 package.scripts['build:build'] = `hackmyresume BUILD ./dist/tmp.json TO ./dist/${name}.html ./dist/${name}.pdf -t compact`;
 package.scripts['build:clean'] = `rm ./dist/compact-html.css && rm ./dist/compact-pdf.css && rm ./dist/${name}.pdf.html && rm ./dist/tmp.json`;
 
